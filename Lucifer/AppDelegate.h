c1c6c97b600fcbd78148781f8fb234ea1cff185f
Flávio Caetano <flavio@vieiracaetano.com>
2015-06-23 20:07:23 -0300

    Initial Commit

---
+++ Lucifer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lucifer
+//
+//  Created by Flávio Caetano on 6/23/15.
+//  Copyright (c) 2015 Shufflow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Lucifer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Lucifer
+//
+//  Created by Flávio Caetano on 6/23/15.
+//  Copyright (c) 2015 Shufflow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
